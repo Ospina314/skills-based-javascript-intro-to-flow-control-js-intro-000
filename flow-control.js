25cd1931a0a4a2420e88f642118a3858c2ba6743
Jonathan Ospina <worldrenaissance@live.com>
2020-10-07 01:22:19 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,32 @@
-function basicTeenager(age) {
 
+function basicTeenager(age) {
+if (age >= 13 && age <= 19) {
+return("You are a teenager!") }
 }
+
+
+
 
 function teenager(age) {
-
+if (age >= 13 && age <= 19){
+return("You are a teenager!")
+} else {
+  return("You are not a teenager")
 }
+}
+
 
 function ageChecker(age) {
-
+  if (age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18 || age === 19){
+  return("You are a teenager!")
+} else if (age =< 12) {
+  return("You are a kid")
+} else {
+  return("You are a grownup")
 }
+}
+
+
 
 function ternaryTeenager(age) {
 
