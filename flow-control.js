e12b450920b145a77bc031ce4ab97adbe9e41ef2
Jonathan Ospina <worldrenaissance@live.com>
2020-10-07 02:45:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,11 +32,11 @@ function ternaryTeenager(age) {
 return (age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager")
 }
 
-function switchAge(age)
+function switchAge(age) {
   switch(age) {
     case age === 13 || age === 14 || age === 15 || age === 16 || age === 17 || age === 18 || age === 19 :
     return "You are a teenager"
-    break;
     default:
     return "You have an age"
     }
+  }
